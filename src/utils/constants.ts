df318e73d3126a631903bd7ad1535e4b08c1b41f
Swastik Goswami <cwswastik2005@gmail.com>
2025-03-22 11:09:05 +0530

    fix: endpoint

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,3 +2,4 @@ export const backendUrl: string =
   (import.meta.env.VITE_BACKEND_URL as string) ?? "http://localhost:8000";
 
 export const generateSessionEndpoint = `/session/generate`;
+export const generateVoiceEndpoint = `/session/generate-voice`;
