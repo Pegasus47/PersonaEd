6125b15a12d8e4901775bb7a54afbce545fdaaf3
Swastik Goswami <cwswastik2005@gmail.com>
2025-03-22 15:55:16 +0530

    feat: fix links

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,7 @@
 export const backendUrl: string =
   (import.meta.env.VITE_BACKEND_URL as string) ?? "http://localhost:8000";
 
-export const generateSessionEndpoint = `/session/generate`;
-export const generateVoiceEndpoint = `/session/generate-voice`;
-export const charactersEndpoint = `/characters`;
-export const pdfEndpoint = `/session/upload-pdf`;
+export const generateSessionEndpoint = `/session/generate/`;
+export const generateVoiceEndpoint = `/session/generate-voice/`;
+export const charactersEndpoint = `/characters/`;
+export const pdfEndpoint = `/session/upload-pdf/`;
